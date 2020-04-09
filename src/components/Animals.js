296a828dfb7d3b9e9ab9b2a50cbc7ce832b75f7e
Jonathan Drew <jonathan.drew@codeimmersives.com>
2020-04-09 15:52:09 -0400

    still working on map

--- src/components/Animals.js
+++ src/components/Animals.js
@@ -1,14 +1,90 @@
 import React, { Component } from "react";
+let images = [
+  {
+    image: "/images/canines/candog.jpeg",
+    type: "",
+    name: "",
+    description: "dog",
+    animalId: "",
+  },
+  {
+    image: "/images/canines/canfox.jpeg",
+    type: "",
+    name: "",
+    description: "fox",
+    animalId: "",
+  },
+  {
+    image: "/images/canines/canwolf.jpeg",
+    type: "",
+    name: "",
+    description: "wolf",
+    animalId: "",
+  },
+  {
+    image: "/images/feline/felcat.jpeg",
+    type: "",
+    name: "",
+    description: "cat",
+    animalId: "",
+  },
+  {
+    image: "/images/feline/feljag.jpeg",
+    type: "",
+    name: "",
+    description: "jaguar",
+    animalId: "",
+  },
+  {
+    image: "/images/feltiger.jpeg",
+    type: "",
+    name: "",
+    description: "tiger",
+    animalId: "",
+  },
+  {
+    image: "/images.primates/primape",
+    type: "",
+    name: "",
+    description: "ape",
+    animalId: "",
+  },
+  {
+    image: "/images/primates/primonk",
+    type: "",
+    name: "",
+    description: "monkey",
+    animalId: "",
+  },
+  {
+    image: "/images/primates/primrilla",
+    type: "",
+    name: "",
+    description: "gorilla",
+    animalId: "",
+  },
+];
+// function searchIt(term) {
+//   return function (item) {
+//     return item.subject.toLowerCase().includes(term.toLowerCase());
+//   };
+// }
 class Animals extends Component {
-  constructor(props) {
-    super(props);
-    this.state = [];
+  constructor() {
+    super();
+    this.state = { images };
   }
   render() {
     return (
       <div>
-        test test test test
-        <img src="/images/canines/candog.jpeg" alt="..."></img>
+        {this.state.images.map((item) => {
+          <div>
+            {" "}
+            {item.description}
+            {/* <p>{item.description}</p> */}
+            {/* <img src={item.image}></img> */}
+          </div>;
+        })}
       </div>
     );
   }
